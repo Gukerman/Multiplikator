95c50e11487e29908a76d15bed2e1c235cda7907
Gukerman <guk_a@ufanet.ru>
2018-07-05 14:42:58 +0500

    4.2
    Добавил EPROM
    Это второй станок с EPROM , первый забыл вылить.

--- Multiplikator/Multiplikator.ino
+++ Multiplikator/Multiplikator.ino
@@ -1,19 +1,21 @@
 /*
  * Кабельный мультипликатор
+ * 
+ * Станок номер 5, сборка Наиля внутри шкафа, шаг 1/8
+ * 
 */
 
+#include <EEPROM.h>
 #include <LiquidCrystal.h>                // Подключяем библиотеку
 LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );    // Указываем порты         
 
+//#define DEBUG //uncomment if you want  debug
+
 //Определяем переменные длительность импульса и задержка
-  int d=100;  // пауза в микросекундах между импульсами на мотор
+  int d=66;  // пауза в микросекундах между импульсами на мотор
   bool revers;
     
 //Определяем порты подключенные к ШД
-//int PUL=8; //define Pulse pin
-//int DIR=9; //define Direction pin
-//int ENA=10; //define Enable Pin
-
 // Первый станок
  int PUL=3; //define Pulse pin
  int DIR=12; //define Direction pin
@@ -44,9 +46,12 @@ long mil_summ=0;
 
 void setup()
 {
+  mm = EEPROM.read(1);
+
   pinMode (PUL, OUTPUT);
   pinMode (DIR, OUTPUT);
   pinMode (ENA, OUTPUT);
+  digitalWrite(ENA,HIGH);
   
   pinMode(port_l,INPUT);
   pinMode(port_r,INPUT);
@@ -61,6 +66,7 @@ void setup()
   lcd.print(mm/10);                        // Вывести скорость
   lcd.print("mm");                        // Вывести скорость
   Serial.begin(9600);                     // Включаем последовательный порт
+
 
 }
  
@@ -89,28 +95,47 @@ if ((s_mm < mm)&&(gerkon == 1))
   if (revers) back();
   else forward();
   s_mm ++;
+  #ifdef DEBUG
 //    Serial.println(s_mm); 
+  #endif
   }
 else 
+
   {
-  if ( 333 < (millis()-dreb_g))   
+  if ( 100 < (millis()-dreb_g))   
     {
     s_mm = 0;
     gerkon = 0;
     }
   }
 
-// gerkon = 1;
-
-if (digitalRead(port_l)==LOW) 
+ 
+    #ifdef DEBUG
+//      gerkon = 1;
+    #endif
+    
+if (digitalRead(port_l)==LOW) key_l++;
+ else  key_l=0;
+ 
+if (key_l>2)
   {
     revers=1;
-//      Serial.println("Left"); 
+      
+    #ifdef DEBUG
+      Serial.println("Left"); 
+    #endif
   }
-if (digitalRead(port_r)==LOW) 
+
+if (digitalRead(port_r)==LOW) key_r++;
+ else  key_r=0;
+   
+if (key_r>2)
   {
     revers=0;
-      //Serial.println("Rihgt"); 
+    
+    #ifdef DEBUG
+      Serial.println("Rihgt"); 
+    #endif
   }
 
   x=analogRead (0);
@@ -126,6 +151,8 @@ if (digitalRead(port_r)==LOW)
         
         else if (x < 200) {                     // +
         mm++;
+        EEPROM.write(1, mm);
+            ////resetFunc();
         lcd.setCursor(9,1);                    // Установить курсор на конец строки
         lcd.print("             ");              // Очистка экрана
         lcd.setCursor(9,1);                    // Установить курсор на конец строки
@@ -134,7 +161,12 @@ if (digitalRead(port_r)==LOW)
         dreb = millis();
       }
         else if (x < 400){                      // -
-        if (mm >= 0.1) mm--;
+        if (mm >= 0.1) 
+        {
+          mm--;
+          EEPROM.write(1, mm);
+              ////resetFunc();
+        }
         lcd.setCursor(9,1);                    // Установить курсор на конец строки
         lcd.print("             ");              // Очистка экрана
         lcd.setCursor(9,1);                    // Установить курсор на конец строки
